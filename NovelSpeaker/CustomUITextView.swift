dd7d8b39f887a955cbbb9a914f0f82d4939b7a9e
IIMURA Takuji <uirou.j@gmail.com>
2019-11-13 04:14:40 +0900

    Xcode 11.2 で UITextView を Storyboard で使うと落ちる問題へのworkaround対応(そのままリリースはできない)

--- NovelSpeaker/CustomUITextView.swift
+++ NovelSpeaker/CustomUITextView.swift
@@ -11,6 +11,26 @@ import UIKit
 import RealmSwift
 
 public class CustomUITextView: UITextView {
+    public required init?(coder: NSCoder) {
+        if #available(iOS 13.2, *) {
+            super.init(coder: coder)
+        }
+        else {
+            super.init(frame: .zero, textContainer: nil)
+            self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            self.contentMode = .scaleToFill
+
+            self.isScrollEnabled = false   // causes expanding height
+
+            // Auto Layout
+            self.translatesAutoresizingMaskIntoConstraints = false
+            self.font = UIFont(name: "HelveticaNeue", size: 18)
+
+            // custom setting.
+            self.isEditable = false
+        }
+    }
+    
     // UITextView で長押しして出て来るメニューの項目を減らします
     // from http://qiita.com/watt1006/items/2425bfa1720d522d05fd
     override public func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
