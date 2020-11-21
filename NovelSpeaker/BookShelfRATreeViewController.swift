e69e21c94eaf2bed6e9fd2fbf025490b92b727c6
IIMURA Takuji <uirou.j@gmail.com>
2020-11-22 01:46:17 +0900

    ダークモードでも背景色が変わっていなかった問題を修正

--- NovelSpeaker/BookShelfRATreeViewController.swift
+++ NovelSpeaker/BookShelfRATreeViewController.swift
@@ -69,6 +69,10 @@ class BookShelfRATreeViewController: UIViewController, RATreeViewDataSource, RAT
         treeView.rowHeight = UITableView.automaticDimension
         self.treeView = treeView
         
+        if #available(iOS 13.0, *) {
+            self.view.backgroundColor = UIColor.systemBackground
+        }
+        
         // このタイミングで StorySpeaker のインスタンスを作っておきます。
         // Realm observe が走るので、main thread で作っておかねばならぬらしい(´・ω・`)
         _ = StorySpeaker.shared
