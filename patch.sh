3d72d2778eca68c9eb08f52fcae8e6640f558f26
IIMURA Takuji <uirou.j@gmail.com>
2026-08-23 15:10:00 +0900

    assert で死んでしまうのは悲しいのでなんとかするやつです

--- patch.sh
+++ patch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 (cd Pods/IceCream/IceCream/Classes/; patch BackgroundWorker.swift ../../../../IceCream_BackgroundWorker.swift.patch)
+patch Pods/IceCream/IceCream/Classes/CKRecordConvertible.swift IceCream_CKRecordConvertible.swift.patch
 #(cd Pods/RATreeView/RATreeView/RATreeView/Private\ Files; patch RATreeView+Enums.m ../../../../../RATreeView_RATreeView+Enums.m.patch)
